50bfe8bc71533b44eb987ce27d55e23e5aea5aaf
jikstra <jikstra@disroot.org>
2018-09-18 20:41:35 +0200

    Here we go

---
+++ index.js
@@ -0,0 +1,136 @@
+function mapComposables(gulp, composables) {
+  return composables.map(c => composeIfPossible(gulp, c))
+}
+
+function composeIfPossible(gulp, composable) {
+  return composable instanceof gulpComposeComposable ? composable.compose(gulp) : composable
+}
+
+class gulpComposeComposable {
+  constructor() {
+  }
+
+  compose (gulp) {
+    throw new Error('Not implemented')
+  }
+}
+
+class gulpComposeSeries extends gulpComposeComposable{
+  constructor(...tasks) {
+    super()
+    this.tasks = tasks
+  }
+
+  compose(gulp) {
+    return gulp.series(...mapComposables(gulp, this.tasks))
+  }
+}
+
+class gulpComposeParallel extends gulpComposeComposable {
+  constructor(...tasks) {
+    super()
+    this.tasks = tasks
+  }
+
+    compose(gulp) {
+      return gulp.parallel(...mapComposables(gulp, this.tasks))
+    }
+}
+
+class gulpComposeSrc extends gulpComposeComposable {
+  constructor(globs, options) {
+    super()
+    this.globs = globs
+    this.options = options ? options : {}
+    this.pipes = []
+  }
+
+  pipe(fn) {
+    this.pipes.push(fn)
+    return this
+  }
+
+  compose(gulp) {
+    let gulpSource = gulp.src(this.globs, this.options)
+    for(let pipe of this.pipes) {
+      if(pipe instanceof gulpComposeComposable) {
+        gulpSource = gulpSource.pipe(pipe.compose())
+      } else {
+        gulpSource = gulpSource.pipe(pipe)
+      }
+    }
+    return gulpSource
+  }
+}
+
+class gulpComposeDest extends gulpComposeComposable {
+  constructor(path, options) {
+    this.path = path
+    this.options = options
+  }
+
+  compose(gulp) {
+    return gulp.dest(this.path, this.options)
+  }
+}
+
+class gulpComposeWatch extends gulpComposeComposable {
+  //TODO: Implement .on stuff
+  constructor(globs, options, fns) {
+    super()
+    this.globs = globs
+    if(!fn) {
+      fns = options
+      options = undefined
+    }
+    this.options = options
+    this.fns = fns
+  }
+
+  compose(gulp) {
+    return gulp.watch(this.globs, this.options, this.fns)
+  }
+}
+
+
+
+class gulpCompose {
+  constructor(gulp) {
+    this.gulp = gulp ? gulp : require('gulp')
+    this.tasks = {}
+  }
+
+  src(globs, options) {
+    return new gulpComposeSrc(globs, options)
+  }
+
+  dest(path, options) {
+    return new gulpComposeDest(dest, options)
+  }
+
+  watch(globs, options, fns) {
+    return new gulpComposeWatch(globs, options, fns)
+  }
+  series(...tasks) {
+    return new gulpComposeSeries(...tasks)
+  }
+
+  parallel(...tasks) {
+    return new gulpComposeParallel(...tasks)
+  }
+
+  task(name, fn) {
+    this.tasks[name] = fn
+  }
+
+  compose() {
+    for(let task in this.tasks) {
+      let fn = composeIfPossible(this.gulp, this.tasks[task])
+      this.gulp.task(task, fn)
+    }
+
+    return this.gulp
+  }
+}
+
+module.exports = gulpCompose
---
+++ test.js
@@ -0,0 +1,156 @@
+const bach = require('bach')
+const gulpCompose = require('./index')
+const tape = require('tape')
+
+tape('should compose an series task', t => {
+  let gc = new gulpCompose()
+  let taskName = 'test'
+  let task1Called = false
+  let task2Called = false
+  let tasks = [
+    done => {
+      task1Called = true
+      done()
+    },
+    done => {
+      task2Called = true
+      done()
+    }
+  ]
+
+  gc.task(taskName, gc.series(...tasks))
+  t.ok(gc.tasks['test'], 'test task should exists')
+  let gulp = gc.compose()
+  t.ok(gulp._registry._tasks.test.unwrap().displayName == '<series>', 'unwrapped task should be an series task')
+  gulp.task('test')(done => {
+    t.ok(task1Called, 'task1 got called')
+    t.ok(task2Called, 'task2 got called')
+    t.end()
+  })
+})
+
+tape('should compose an parallel task', t => {
+  let gc = new gulpCompose()
+  let taskName = 'test'
+  let task1Called = false
+  let task2Called = false
+  let tasks = [
+    done => {
+      task1Called = true
+      done()
+    },
+    done => {
+      task2Called = true
+      done()
+    }
+  ]
+
+  gc.task(taskName, gc.parallel(...tasks))
+  t.ok(gc.tasks['test'], 'test task should exists')
+  let gulp = gc.compose()
+  t.ok(gulp._registry._tasks.test.unwrap().displayName == '<parallel>', 'unwrapped task should be an parallel task')
+  gulp.task('test')(done => {
+    t.ok(task1Called, 'task1 got called')
+    t.ok(task2Called, 'task2 got called')
+    t.end()
+  })
+})
+
+tape('should compose a series task which includes another parallel task', t => {
+  let gc = new gulpCompose()
+  let taskName = 'test'
+  let task1Called = false
+  let task2Called = false
+  let tasks = [
+    done => {
+      task1Called = true
+      done()
+    },
+    done => {
+      task2Called = true
+      done()
+    }
+  ]
+
+  let task3Called = false
+  let tasks2 = [
+    done => {
+      task3Called = true
+      done()
+    },
+    'test'
+  ]
+  gc.task(taskName, gc.parallel(...tasks))
+  gc.task('test2', gc.series(...tasks2))
+
+  let gulp = gc.compose()
+
+  gulp.task('test2')(done => {
+    t.ok(task1Called, 'task1 got called')
+    t.ok(task2Called, 'task2 got called')
+    t.ok(task2Called, 'task3 got called')
+
+    t.end()
+  })
+})
+
+tape('gulpCompose.src should return a gulpComposeSrc instance', t => {
+  let gc = new gulpCompose()
+
+  let task1Called = false
+  let task1Fn = done => {
+    task1Called = true
+    done()
+  }
+
+  let task2Called = false
+  let task2Fn = done => {
+    task2Called = true
+    done()
+  }
+
+  let globs = ['./test/**.js', '!./test/**.jsx']
+  let gcSrc = gc.src(globs).pipe(task1Fn).pipe(task2Fn)
+  t.ok(gcSrc.globs == globs, 'globs should be the same')
+  t.ok(Object.keys(gcSrc.options).length === 0 && gcSrc.options.constructor === Object, 'options should be empty object')
+  t.deepLooseEqual(gcSrc.pipes, [task1Fn, task2Fn], 'should contain piped tasks in same order')
+
+  let options = {foo:'bar'}
+  gcSrc = gc.src(globs, options).pipe(task1Fn).pipe(task2Fn)
+  t.ok(gcSrc.globs == globs, 'globs should be the same')
+  t.equals(gcSrc.options, options, 'options should be the same')
+  t.end()
+})
+
+tape('gulpCompose.compose should compose more complex tasks', t => {
+  let gc = new gulpCompose()
+
+  let task1Called = false
+  let task1Fn = done => {
+    task1Called = true
+    done()
+  }
+
+  let task2Called = false
+  let task2Fn = done => {
+    task2Called = true
+    done()
+  }
+
+
+  let task3Called = false
+  let task3Fn = done => {
+    task3Called = true
+    done()
+  }
+
+  gc.task('test', gc.series(gc.parallel(task1Fn, task2Fn), gc.series(task3Fn)))
+  let gulp = gc.compose()
+  gulp.task('test')(done => {
+    t.ok(task1Called, 'task1 got called')
+    t.ok(task2Called, 'task2 got called')
+    t.ok(task2Called, 'task3 got called')
+
+    t.end()
+  })
+})
